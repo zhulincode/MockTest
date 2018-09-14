6da22c4137ee8af93a78a3ec2d02fc1fb0543dc6
zhulincode <43241270+zhulincode@users.noreply.github.com>
2018-09-14 20:42:50 +0800

    Add files via upload

---
+++ MocoTest/HttpClient/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -0,0 +1,34 @@
+package com.course.httpclient.cookies;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MyCookiesForGet {
+    private String url;
+    private ResourceBundle bundle;  //读取配置文件
+
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+        url = bundle.getString("test.url");
+    }
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        String url = bundle.getString("getCookies.url");
+        HttpGet get = new HttpGet(this.url);
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
